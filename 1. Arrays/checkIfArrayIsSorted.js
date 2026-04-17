e0cf022c3d39ecb8bcc87c2914ef9dffb383fb39
Shrinivas <jshrinivas1996@gmail.com>
2026-04-17 23:54:57 +0530

    Check if array is sorted

---
+++ 1. Arrays/checkIfArrayIsSorted.js
@@ -0,0 +1,10 @@
+// Check if array is sorted
+
+function isArraySorted(arr){
+    for(let i = 0; i < arr.length - 1; i++){
+        if(arr[i] > arr[i + 1]){
+            return false;
+        }
+    }
+    return true;
+}
