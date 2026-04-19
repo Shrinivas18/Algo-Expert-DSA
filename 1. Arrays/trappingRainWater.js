8130193e4e49c8fa79162becb1e3d735fbe62718
Shrinivas <jshrinivas1996@gmail.com>
2026-04-19 23:49:48 +0530

    Trapping Rain Water problem

---
+++ 1. Arrays/trappingRainWater.js
@@ -0,0 +1,32 @@
+// Trapping Rain Water problem
+
+function trap(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let leftMax = 0;
+    let rightMax = 0;
+    let water = 0;
+
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                water += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                water += rightMax - height[right];
+            }
+            right--;
+        }
+    }
+
+    return water;
+}
+
+// Example
+console.log(trap([4, 2, 0, 3, 2, 5])); // 9
