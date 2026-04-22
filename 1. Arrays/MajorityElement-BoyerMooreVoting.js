9993b4b26864822fed643038701a9b3b26c85ed9
Shrinivas <jshrinivas1996@gmail.com>
2026-04-22 23:50:46 +0530

    Majority Element (Boyer-Moore Voting)

---
+++ 1. Arrays/MajorityElement-BoyerMooreVoting.js
@@ -0,0 +1,18 @@
+// Majority Element (Boyer-Moore Voting)
+
+function majorityElement(nums) {
+    let count = 0;
+    let candidate = null;
+
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+
+        count += (num === candidate) ? 1 : -1;
+    }
+
+    return candidate;
+}
+
+console.log(majorityElement([3, 2, 3])); // 3
