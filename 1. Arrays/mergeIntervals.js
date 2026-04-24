21434ca1ecd50d3f22afbb7f75b07bafae135e16
Shrinivas <jshrinivas1996@gmail.com>
2026-04-24 23:15:26 +0530

    Merge Intervals

---
+++ 1. Arrays/mergeIntervals.js
@@ -0,0 +1,25 @@
+// Merge Intervals
+
+function mergeIntervals(intervals) {
+    if (intervals.length <= 1) return intervals;
+
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    const result = [];
+    let current = intervals[0];
+
+    for (let i = 1; i < intervals.length; i++) {
+        let next = intervals[i];
+
+        if (next[0] <= current[1]) {
+            current[1] = Math.max(current[1], next[1]);
+        } else {
+            result.push(current);
+            current = next;
+        }
+    }
+
+    result.push(current);
+
+    return result;
+}
