e5e7e6d10c0f3f7341572ee193c135cd3cbd34a9
Shrinivas <jshrinivas1996@gmail.com>
2026-04-25 23:42:08 +0530

    Prefix Sum

---
+++ 1. Arrays/prefixSum.js
@@ -0,0 +1,11 @@
+// Prefix Sum
+function buildPrefixSum(nums) {
+    let prefix = new Array(nums.length);
+    prefix[0] = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        prefix[i] = prefix[i - 1] + nums[i];
+    }
+
+    return prefix;
+}
